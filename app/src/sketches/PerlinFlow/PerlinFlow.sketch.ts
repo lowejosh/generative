40687b3cbb2669f088f7226917acf3d7d74128c8
josh <lowejosh50@gmail.com>
2020-11-18 11:47:55 +1000

    progress - needs tweaking

--- app/src/sketches/PerlinFlow/PerlinFlow.sketch.ts
+++ app/src/sketches/PerlinFlow/PerlinFlow.sketch.ts
@@ -1,10 +1,9 @@
 import { initialPerlinFlowVars, PerlinFlowVars } from "./PerlinFlow.variables";
 import { checkForMismatchedSize } from "utils/misc/checkForMismatchedSize";
+import { createParticle, Particle } from "factories/Particle";
 import { getVectorFromAngle } from "utils/data/vectors";
 import { P5Instance } from "types/p5";
-import { createParticle, Particle } from "factories/Particle";
-import { Vector } from "p5";
-import { NEON_PINK } from "./../../constants/colors";
+import p5, { Vector } from "p5";
 
 const BASE_INCREMENT = 0.01;
 const MAX_RANDOM_SEED = 1000000;
@@ -14,7 +13,7 @@ export const getPerlinFlowSketch = () => {
     p.variables = initialPerlinFlowVars;
     let initOffset: number;
     let zOff = 0;
-    const particleAmount = 1;
+    const particleAmount = 400;
     const particles: Array<Particle> = [];
     const vectorForceDivisor = 6;
 
@@ -74,8 +73,6 @@ export const getPerlinFlowSketch = () => {
         let yOff = initOffset;
 
         const forceVectors: Array<Array<Vector>> = [];
-        const cols = Math.floor(p.width / vectorPadding);
-        const rows = Math.floor(p.height / vectorPadding);
 
         for (let x = 0; x < p.width + vectorPadding; x += vectorPadding) {
           const col = Math.floor(x / vectorPadding);
@@ -98,36 +95,39 @@ export const getPerlinFlowSketch = () => {
               (y > centerY ? (y - centerY) / centerY : 1 - y / centerY) * 100
             );
             // next is to use these percentage values to make the vector point more and more to the center as the percentage goes up
-            // create a vector that points to the center
-            const positionVector = p.createVector(x, y);
+            // create a vector that points to the center by dividing the difference between the position and the center coordinates (by the forceDivisor)
+            // this will also scale it so the outer vectors will be stronger because the divisor remains constant as the distance grows (which is what we want)
+            // this effect is also furtherly compounded with multiplying by the normalized percentage difference between the two points to allow for a nice gradient
+            // transition between the two vectors to allow for less influence by the toCenterVector in places where its not necessary (near the center, where we want
+            // most of the effect to be from the perlin noise)
             const centerVector = p.createVector(centerX, centerY);
-            const angleToCenter = centerVector.angleBetween(positionVector);
-            if (p.frameCount % 180 === 0) {
-              console.log(positionVector.x, positionVector.y);
-              console.log(centerVector.x, centerVector.y);
-            }
-            const vectorPointingToCenter = getVectorFromAngle(
-              x,
-              y,
-              angleToCenter,
-              vectorPadding
+            const toCenterVector = p.createVector(
+              centerVector.x - x,
+              centerVector.y - y
             );
+            toCenterVector.div(vectorForceDivisor);
+            toCenterVector.x =
+              (toCenterVector.x * percentageFromCenterX) / 1000;
+            toCenterVector.y =
+              (toCenterVector.y * percentageFromCenterY) / 1000;
 
-            p.stroke(255, 0, 0);
-            p.ellipse(positionVector.x, positionVector.y, 2, 2);
-            p.stroke(0, 255, 0);
-            p.ellipse(vectorPointingToCenter.x, vectorPointingToCenter.y, 2, 2);
+            // find the difference between the forcevector and tocentervector and use the percentage difference to
 
             // push the vector to the multiarray
             forceVectors[col].push(
-              forceVector.copy().sub(x, y).div(vectorForceDivisor)
+              forceVector.sub(x, y).div(vectorPadding).add(toCenterVector)
             );
 
             // // debug
-            if (percentageFromCenterX > 5 || percentageFromCenterY > 5) {
+            if (percentageFromCenterX > 10 || percentageFromCenterY > 10) {
               p.stroke(255, 100, 100, 50);
             }
-            p.line(x, y, vectorPointingToCenter.x, vectorPointingToCenter.y);
+            p.line(
+              x,
+              y,
+              x + forceVector.x * vectorPadding,
+              y + forceVector.y * vectorPadding
+            );
             p.stroke(255, 255, 255);
 
             yOff += perlinYIncrementScale * BASE_INCREMENT;
@@ -142,28 +142,14 @@ export const getPerlinFlowSketch = () => {
           const row = Math.floor(particle.location.y / vectorPadding);
           const forceVector = forceVectors?.[col]?.[row];
 
-          // const angle =
-          //   (p.noise(
-          //     particle.location.x / 1000,
-          //     particle.location.y / 1000,
-          //     zOff
-          //   ) *
-          //     p.TWO_PI *
-          //     angleVariation) %
-          //   p.TWO_PI;
-
-          // const forceVector = getVectorFromAngle(
-          //   particle.location.x,
-          //   particle.location.y,
-          //   angle,
-          //   vectorPadding
-          // );
-
+          // apply force vector if it exists
           if (forceVector) {
             particle.acceleration.mult(0);
             particle.velocity.mult(0);
             particle.applyForce(forceVector.copy());
           }
+
+          // update values and draw the particle
           particle.update(p);
           particle.display(p);
         });
