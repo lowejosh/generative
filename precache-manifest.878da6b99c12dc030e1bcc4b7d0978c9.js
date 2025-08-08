629c1dcdaf74dc3a3a2b7a364a38d918aada1f60
Joshua Lowe <lowejosh50@gmail.com>
2025-08-08 10:49:57 +1000

    Updates

---
+++ precache-manifest.878da6b99c12dc030e1bcc4b7d0978c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61f0ee9af6bfaf717d054ceefb132f0",
+    "url": "/generative/index.html"
+  },
+  {
+    "revision": "eec3d8fb4c743188487a",
+    "url": "/generative/static/js/2.8e35bd30.chunk.js"
+  },
+  {
+    "revision": "53dcb6d2a0490b49c4ad357697eb2f13",
+    "url": "/generative/static/js/2.8e35bd30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325c8bd76842f7918f53",
+    "url": "/generative/static/js/main.006a9c21.chunk.js"
+  },
+  {
+    "revision": "4f4b29e13b9a5eead263",
+    "url": "/generative/static/js/runtime-main.4b9fda25.js"
+  }
+]);
